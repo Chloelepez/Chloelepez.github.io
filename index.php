4415f1e447829d2d4962f85bee5099dab6517439
Chloelepez <ULB@P-92270122.admin.ulb.priv>
2018-02-16 13:07:13 +0100

    contact
    form

--- index.php
+++ index.php
@@ -877,35 +877,35 @@
               <div class="col-md-9 content-right">
                 <?php
       					if(isset($_POST['submit'])){
-      							$to = "info@metamarbleandgranite.com"; // this is your Email address
+      							$to = "ChloeLepez@gmail.com"; // this is your Email address
       							$from = $_POST['email']; // this is the sender's Email address
       							$first_name = $_POST['first_name'];
-      							$last_name = $_POST['phone'];
-      							$subject = "Meta website question";
+      							$subject = "Portfolio question";
       							$subject2 = "Copy of your form submission";
-      							$message = $first_name . " " . $phone . "  wrote the following:" . "\n\n" . $_POST['message'];
-      							$message2 = "Mail Sent. Thank you for contacting Meta Granite and Marble " . $first_name . ", we will contact you shortly." . "\n" ."Here is a copy of your message " . "\n\n" . $_POST['message'];
+      							$message = $first_name . "  wrote the following:" . "\n\n" . $_POST['message'];
+      							$message2 = "Mail Sent. Thank you for contacting me " . $first_name . ", I will contact you shortly." . "\n" ."Here is a copy of your message " . "\n\n" . $_POST['message'] . "\n\n" . "Chloé Lepez";
 
       							$headers = "From:" . $from;
       							$headers2 = "From:" . $to;
       							mail($to,$subject,$message,$headers);
       							mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-      							echo "<p>Mail Sent. Thank you " . $first_name . ", we will contact you shortly.</p>";
+      							echo "<p>Mail Sent. Thank you " . $first_name . ", I will contact you shortly.</p>";
       							}
       					?>
 
-                <form action="mailto:chloelepez@gmail.com" method="post" enctype="text/plain">
+                <form action="" method="post" enctype="text/plain">
                   <div class="group">
-                    <input required="" type="text"><span class="highlight"></span><span class="bar"></span><label>Name</label>
+                    <input required="" type="text"  name="first_name"><span class="highlight"></span><span class="bar"></span><label>Name</label>
                   </div>
                   <div class="group">
-                    <input required="" type="email"><span class="highlight"></span><span class="bar"></span><label>Email</label>
+                    <input required="" type="email"  name="email"><span class="highlight"></span><span class="bar"></span><label>Email</label>
                   </div>
                   <div class="group">
                     <textarea required=""></textarea><span class="highlight"></span><span class="bar"></span><label>Message</label>
                   </div>
-                  <input id="sendMessage"  type="submit" value="Send">
+                  <input id="sendMessage" type="submit" value="Submit" name="submit">
                 </form>
+
               </div>
             </div>
           </div>
